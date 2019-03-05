5b5a99bce020c04feaef7a0338dadd240fe07557
danbst <abcz2.uprola@gmail.com>
2019-03-05 04:48:46 +0200

    Moved because of https://github.com/NixOS/nixpkgs/pull/55867 Upstream doesn't care about stableness and reproducibility but this module can still be used as part of another, more declarative process

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,7 @@
 {
-  # Add your NixOS modules here
-  #
-  # my-module = ./my-module;
+  # Moved because of https://github.com/NixOS/nixpkgs/pull/55867
+  # Upstream doesn't care about stableness and reproducibility
+  # but this module can still be used as part of another, more declarative process
+  geoip-updater = ./geoip-updater;
 }
 
